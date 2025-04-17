9b9dd98218f2cc1811e7e9b7ea7fe22b348a856f
lillaconte <137272817+lillaconte@users.noreply.github.com>
2025-04-17 15:17:39 +0200

    add gliner test script

---
+++ gliner_test.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from gliner import GLiNER
+
+# gliner multilingual model
+model = GLiNER.from_pretrained("urchade/gliner_multi_pii-v1")
+
+# annotated dataset
+df_annoté = pd.read_csv('/home/lilla/retronews/annotated_dataset_deduped.csv')
+
+# full dataset
+df_full = pd.read_csv('/home/lilla/retronews/sample_fulldataset.csv')
+
+# location label
+labels = ["location"]
+
+# extract town/city and region
+def extract_city_and_region(text):
+    # detect location types
+    entities = model.predict_entities(text, labels)
+    city = None
+    region = None
+
+    for entity in entities:
+        if entity["label"] == "location":
+            city = entity["text"]
+            region = entity["text"]
+    
+    return city, region
+
+# apply function
+df_full['event_ville'], df_full['event_region'] = zip(*df_full['text'].apply(extract_city_and_region))
+
+# output file
+df_full.to_csv('gliner_full_dataset.csv', index=False)
+
